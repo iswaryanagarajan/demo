260e040a397c3b8e8cb36f8226fb9a1e4c66f560
Iswarya <iswaryanagarajan28@gmail.com>
2019-08-09 13:59:38 +0530

    ja

--- src/demo1/new2.java
+++ src/demo1/new2.java
@@ -1,10 +1,14 @@
 package demo1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
 public class new2 {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.ie.driver","C:\\Users\\LKB-L-039\\Downloads\\IEDriverServer_Win32_3.14.0.zip\\IEDriverserver.exe");
-		WebDriver driver= new InternetExplorerDriver();
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\LKB-L-039\\Desktop\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
 		driver.get("http://demo.automationtesting.in/");
 	}
 }
